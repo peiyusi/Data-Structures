48eb9a023b3d3507a0113988931a999955b94030
peiyusi <1079986574@qq.com>
2017-08-07 21:44:30 +0800

    delect m

--- 习题3.4最长连续递增子序列.cpp
+++ 习题3.4最长连续递增子序列.cpp
@@ -1,53 +1,37 @@
-#include<stdio.h>  
-#include<string.h>  
-struct node  
-{  
-    int b;  
-    int k;  
-    int m;  
-} s[100010] ;  
-int main()  
-{  
-    int i, j;  
-    int n;  
-    int maxi = 1, c;  
-    scanf("%d", &n);  
-    for (i = 0; i < n; i++)  
-    {  
-        scanf("%d", &s[i].b);  
-    }  
-  
-    for (i = 0; i < n; i++)  
-    {  
-        s[i].k = 1;  
-    }  
-    for (i = 0; i < n - 1; i++)  
-    {  
-        for (j = i + 1; j < n; j++)  
-        {  
-            if (s[j].b > s[j - 1].b)  
-            {  
-                s[i].k++;  
-  
-            }  
-            else  
-            {  
-                break;  
-            }  
-  
-        }  
-    }  
-    for (i = 0; i < n; i++)  
-    {  
-        if (s[i].k > maxi)  
-        {  
-            maxi = s[i].k;  
-            c = i;  
-        }  
-    }  
-    for (i = c; i < c + maxi-1; i++)  
-    {  
-        printf("%d ", s[i].b);  
-    }  
-    printf("%d\n", s[c+maxi-1].b);  
+#include <stdio.h>
+struct LNode{
+	int data;
+	int k;
+}s[100001];
+
+int main() {
+	int n;
+	int i, j;
+	int max = 1, a;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &s[i].data);
+		s[i].k = 1;
+	}
+	for (i = 0; i < n-1; i++) {
+		for (j = i + 1; j < n; j++) {
+			if (s[j].data > s[j-1].data) {
+				s[i].k++;
+			} else {
+				break;
+			}
+		}
+	}
+	for (i = 0; i < n; i++) {
+		if (s[i].k > max) {
+			max = s[i].k;
+			a = i;
+		}
+	}
+	for (i = a; i < a + max-1; i++) {
+		printf("%d ", s[i].data);
+	}
+	printf("%d\n", s[a+max-1].data);
+	
+	return 0;
 }
