a4d439dd9a694c8802243b35a3277b19e9431baf
peiyusi <1079986574@qq.com>
2017-08-26 20:56:13 +0800

    add 3.14

---
+++ 习题3.14另类堆栈.cpp
@@ -0,0 +1,20 @@
+bool Push( Stack S, ElementType X ) {
+	if (S->Top == S->MaxSize) {
+		printf("Stack Full\n");
+		return false;
+	} else {
+		S->Data[S->Top] = X;
+		S->Top++;
+		return true;
+	}
+}
+
+ElementType Pop( Stack S ) {
+	if (S->Top == 0) {
+		printf("Stack Empty\n");
+		return ERROR;
+	} else {
+		S->Top--;
+		return S->Data[S->Top];
+	}
+}
