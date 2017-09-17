86212d273ab4dd723f8a6156a29166f8840d33a3
peiyusi <1079986574@qq.com>
2017-09-17 21:21:34 +0800

    cannot understand

---
+++ MOOC/02-线性结构3 Reversing Linked List.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int list[100010];
+int node[100010][2];
+int main()
+{
+    int st,num,r;
+    cin>>st>>num>>r;
+    int address,data,next,i;
+    for(i=0;i<num;i++)
+    {
+        cin>>address>>data>>next;
+        node[address][0]=data;
+        node[address][1]=next;
+    }
+    int m=0,n=st;
+    while(n!=-1)
+    {
+        list[m++]=n;
+        n=node[n][1];
+    }
+    i=0;
+    while(i+r<=m)
+    {
+        reverse(list+i,list+i+r);
+        i=i+r;
+    }
+    for (i = 0; i < m-1; i++)
+    {
+        printf("%05d %d %05d\n", list[i], node[list[i]][0], list[i+1]);
+    }
+    printf("%05d %d -1\n", list[i], node[list[i]][0]);
+}
--- 浙大数据结构/习题3.6一元多项式的乘法与加法运算.cpp
+++ 浙大数据结构/习题3.6一元多项式的乘法与加法运算.cpp
@@ -224,4 +224,3 @@ int main()
 
     return 0;
 }
-
