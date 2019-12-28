5170c328eab4c475541d69e8eabdf043458c348a
peiyusi <peiyusi@foxmail.com>
2019-12-28 16:02:03 +0800

    add recursive binary search

--- 浙大版《数据结构（第2版）》题目集/习题1.8 二分查找.cpp
+++ 浙大版《数据结构（第2版）》题目集/习题1.8 二分查找.cpp
@@ -14,3 +14,27 @@ Position BinarySearch( List L, ElementType X )
 	}
 	return NotFound;
 }
+
+//recursive
+//Position BinarySearchRecursive( List L, ElementType X, Position left, Position right )
+//{
+//	if (left > right) {
+//		return NotFound;
+//	}
+//	Position mid = (left + right) / 2;
+//	ElementType midData = L->Data[mid];
+//	if (midData == X) {
+//		return mid; 
+//	} else if (midData > X) {
+//		return BinarySearchRecursive(L, X, left, mid - 1);
+//	} else {
+//		return BinarySearchRecursive(L, X, mid + 1, right);
+//	}
+//}
+//
+//Position BinarySearch( List L, ElementType X )
+//{
+//	return BinarySearchRecursive(L, X, 1, L->Last);
+//}
+
+
